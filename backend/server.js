d14d2a58668a3e7a77d24d559a1f04001e1573c1
Dineshd5 <dineshcivilians6@gmail.com>
2025-09-12 20:24:34 +0530

    backend setpup done controllers is going on

---
+++ backend/server.js
@@ -0,0 +1,24 @@
+import cors from "cors";
+import "dotenv/config";
+import express from "express";
+import connectDB from "../backend/config/mongodb.js";
+import connectCloudinary from "./config/cloudinary.js";
+
+// App Config
+const app = express();
+const port = process.env.PORT || 4000;
+connectDB();
+connectCloudinary();
+
+// middlewares
+app.use(express.json());
+app.use(cors());
+
+// API endpoints
+app.get("/", (req, res) => {
+  res.send("API  Working");
+});
+
+app.listen(port, () => {
+  console.log(`server is started on PORT : ${port}`);
+});
