5badc86965a8177892bf4b5d13d510016e8636c8
Dineshd5 <dineshcivilians6@gmail.com>
2025-09-17 09:54:47 +0530

    top-level CORS origin fixed

--- backend/server.js
+++ backend/server.js
@@ -26,7 +26,7 @@ app.use((req, res, next) => {
   res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
   next();
 });
-app.use(cors({ origin: "*", credentials: true }));
+app.use(cors());
 
 // API endpoints
 app.use("/api/user", userRouter);
