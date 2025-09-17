12831e9467b76e091f980238e4b6494b76ffdf8a
Dineshd5 <dineshcivilians6@gmail.com>
2025-09-17 09:44:53 +0530

    top-level CORS origin fixed

--- backend/server.js
+++ backend/server.js
@@ -20,13 +20,13 @@ connectCloudinary();
 
 // middlewares
 app.use(express.json());
-app.use(cors({ origin: "*", credentials: true }));
 app.use((req, res, next) => {
   res.setHeader("Access-Control-Allow-Origin", "*"); // or frontend URL
   res.setHeader("Access-Control-Allow-Methods", "GET,POST,PUT,DELETE,OPTIONS");
   res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
   next();
 });
+app.use(cors({ origin: "*", credentials: true }));
 
 // API endpoints
 app.use("/api/user", userRouter);
