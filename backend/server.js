be0a926c3306b61c7223a8211d1cb4f8faeee75d
Dineshd5 <dineshcivilians6@gmail.com>
2025-09-17 09:15:48 +0530

    cors origin fixed

--- backend/server.js
+++ backend/server.js
@@ -22,13 +22,11 @@ connectCloudinary();
 app.use(express.json());
 app.use(
   cors({
-    origin: function (origin, callback) {
-      if (!origin || allowedOrigins.includes(origin)) {
-        callback(null, true);
-      } else {
-        callback(new Error("Not allowed by CORS"));
-      }
-    },
+    origin: [
+      "http://localhost:5173",
+      "https://forever-frontend-sooty-nine.vercel.app",
+    ],
+    methods: ["GET", "POST", "PUT", "DELETE"],
     credentials: true,
   })
 );
